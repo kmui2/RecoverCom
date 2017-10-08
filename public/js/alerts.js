d268d981645cceaf68351b38c07a698af7091cf7
martinezac9283 <martinezac9283@gmail.com>
2017-10-07 23:09:47 -0500

    Alerts

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -4,8 +4,12 @@ $(document).ready(function(){
     socket.on('message', function (data) {
         console.log('Client said ' + data);
         $('#alerts').html($('#alerts').html() + 
-        `
-        <p> data </p>
+        `<div class="alert alert-dismissible alert-warning">
+        <button class="close" type="button" data-dismiss="alert">&times;</button>
+        <h4>Warning!</h4>
+        <p>Best check yo self, you're not looking too good. Nulla vitae elit libero, a pharetra augue. Praesent commodo cursus magna, <a class="alert-link" href="#">vel scelerisque nisl consectetur et</a>.</p>
+      </div>
+        <p> ${data} </p>
         `);
     })
 
