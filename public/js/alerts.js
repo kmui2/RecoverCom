c845c7207ee3ed6cd2ca5a5b764f383d68511fc3
martinezac9283 <martinezac9283@gmail.com>
2017-10-07 23:58:52 -0500

    Edited Alert Titles

--- public/js/alerts.js
+++ public/js/alerts.js
@@ -1,9 +1,48 @@
 let socket = io.connect();
 $(document).ready(function(){
     console.log('hello')
-    socket.on('message', function (data) {
+    socket.on('message', function (data) { 
+        var messageHtml =``
+        switch (data) { 
+            case "Dressing": 
+                messageHtml = `<div class="alert alert-dismissible alert-success">
+                <button class="close" type="button" data-dismiss="alert">&times;</button>
+                <p> Assist Resident With Dressing </p>
+              </div>`
+                break; 
+
+            case "Bathroom":
+                messageHtml = `<div class="alert alert-dismissible alert-danger">
+                <button class="close" type="button" data-dismiss="alert">&times;</button>
+                <h4>Warning!</h4>
+                <p> Assist Resident With Bathroom <strong>ASAP!</strong> </p>
+              </div>`
+                break;
+
+            case "Transfer":
+                messageHtml = `<div class="alert alert-dismissible alert-warning">
+                <button class="close" type="button" data-dismiss="alert">&times;</button>
+                <p> Assist With Transferring. <p>
+              </div>`
+                break;
+
+            case "Hygeine":
+                messageHtml = `<div class="alert alert-dismissible alert-warning">
+                <button class="close" type="button" data-dismiss="alert">&times;</button>
+                <p> Assist With Personal Hygeine <p>
+              </div>`
+                break;
+
+            case "Other"
+                messageHtml = ` <div class="alert alert-dismissible alert-success">
+                <button class="close" type="button" data-dismiss="alert">&times;</button>
+                <p> Assist Resident. </p>
+              </div>`
+                break;
+        } 
+        
         console.log('Client said ' + data);
-        $('#alerts').html($('#alerts').html() + 
+        $('#alerts').html(  
         `
         <div class="alert alert-dismissible alert-danger">
         <button class="close" type="button" data-dismiss="alert">&times;</button>
@@ -22,7 +61,7 @@ $(document).ready(function(){
       </div>
 
         <p> ${data} </p>
-        `);
+        `+ $('#alerts').html()); 
     })
 
 });
